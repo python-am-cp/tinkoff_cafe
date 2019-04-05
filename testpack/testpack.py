3760492b134136e3793b6fcf7312f5d0b589c7a2
Илья <iLUHA1337@users.noreply.github.com>
2019-04-05 18:02:39 +0300

    Monkey code removed

--- testpack/testpack.py
+++ testpack/testpack.py
@@ -7,10 +7,7 @@ class TestingPackage:
     def f1Metrics(self, predicted, trueAnswer):
         countedPred = {}
         for dish in predicted:
-            if dish in countedPred:
-                countedPred[dish] += 1
-            else:
-                countedPred[dish] = 1
+            countedPred.setdefault(dish, 0) += 1
         intersectSize = 0
         for dish in trueAnswer:
             if dish in countedPred and countedPred[dish] > 0:
@@ -18,7 +15,7 @@ class TestingPackage:
                 intersectSize += 1
         parameterP = intersectSize / len(predicted)
         parameterZ = intersectSize / len(trueAnswer)
-        return 0 if parameterP**2 + parameterZ**2 == 0 else 2 * (parameterP * parameterZ) / (parameterP + parameterZ)
+        return 0 if intersectSize == 0 else 2 * (parameterP * parameterZ) / (parameterP + parameterZ)
     def getTheMetric(self):
         result = .0
         count = min(len(self.features), len(self.labels))
