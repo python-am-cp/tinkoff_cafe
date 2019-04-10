f8b8148d7d40e06290d9b9583c891b11f56d9022
iLUHA1337 <tdr.leet@gmail.com>
2019-04-10 18:21:00 +0300

    Experimental algo

---
+++ devkit/main.py
@@ -0,0 +1,16 @@
+import devkit.data as datapack
+import devkit.mymodel as model
+import testpack.testpack as tp
+data = datapack.Data("data/train.csv", "data/menu_train.csv", "data/menu_tagged.csv")
+model = model.Model(data)
+print("Data was readed and precalculated")
+features = []
+labels = []
+for human in data.getPeopleIds():
+    for check in data.getChecksList(human):
+        features.append([human, check[1], check[2]])
+        labels.append(data.getDishesList(check[0]))
+print("Features and labels was created")
+testpack = tp.TestingPackage(features, labels, model)
+# print("Testing...")
+print(testpack.getTheMetric())
