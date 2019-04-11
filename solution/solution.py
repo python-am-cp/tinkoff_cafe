40428301819349a3ffaf1d63397ef5f632a8a754
Sabalevskiy Sergey <sabal2000@mail.ru>
2019-04-11 23:46:40 +0300

    train and predict scripts done

---
+++ solution/solution.py
@@ -0,0 +1,13 @@
+class Model:
+    def __init__(self):
+        print("Imported")
+
+    def train(self, train, menu_train, goods):
+        print("Trained")
+
+    def load_params(self, menuTestFName='', taggedMenuFName=''):
+        print("Loaded")
+
+    def predict(self, features):
+        print("Predicting")
+        return [12, 13, 8, 124]
